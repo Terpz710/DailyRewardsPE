00c2412681e0ef4d5ab1cd1c43eb69cf9811be4e
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-15 14:17:33 -0800

    Add files via upload

---
+++ DailyRewardsPE/src/Terpz710/DailyRewardsPE/Main.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terpz710\DailyRewardsPE;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use Terpz710\BankNotesPlus\BankNotesPlus;
+use Terpz710\DailyRewardsPE\Commands\DailyCommand;
+use Terpz710\DailyRewardsPE\Commands\WeeklyCommand;
+use Terpz710\DailyRewardsPE\Commands\MonthlyCommand;
+
+class Main extends PluginBase implements Listener {
+
+    /** @var Config */
+    private $config;
+    /** @var Config */
+    private $cooldowns;
+    /** @var Config */
+    private $messages;
+
+    public function onEnable(): void {
+        $this->saveResource("rewards.yml");
+        $this->config = new Config($this->getDataFolder() . "rewards.yml", Config::YAML);
+
+        $this->saveResource("messages.yml");
+        $this->messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
+
+        $cooldownsFolder = $this->getDataFolder() . "cooldowns";
+        if (!is_dir($cooldownsFolder)) {
+            mkdir($cooldownsFolder);
+        }
+        $cooldownsPath = $cooldownsFolder . DIRECTORY_SEPARATOR . "cooldowns.json";
+        $this->cooldowns = new Config($cooldownsPath, Config::JSON);
+
+        $bankNotesPlus = $this->getServer()->getPluginManager()->getPlugin("BankNotesPlus");
+
+        if ($bankNotesPlus !== null) {
+            $this->getServer()->getCommandMap()->register("daily", new DailyCommand($this, $bankNotesPlus));
+            $this->getServer()->getCommandMap()->register("weekly", new WeeklyCommand($this, $bankNotesPlus));
+            $this->getServer()->getCommandMap()->register("monthly", new MonthlyCommand($this, $bankNotesPlus));
+        } else {
+            $this->getLogger()->error("BankNotesPlus plugin not found. Make sure it's installed and enabled.");
+        }
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $this->initializePlayerData($player);
+    }
+
+    public function getRewardsConfig(): Config {
+        return $this->config;
+    }
+
+    public function getCooldowns(): Config {
+        return $this->cooldowns;
+    }
+
+    public function getMessagesConfig(): Config {
+        return $this->messages;
+    }
+
+    private function initializePlayerData($player) {
+        $dailyCooldownKey = "daily_cooldown_" . $player->getName();
+        $weeklyCooldownKey = "weekly_cooldown_" . $player->getName();
+        $monthlyCooldownKey = "monthly_cooldown_" . $player->getName();
+
+        if (!$this->cooldowns->exists($dailyCooldownKey)) {
+            $this->cooldowns->set($dailyCooldownKey, 0);
+            $this->cooldowns->save();
+        }
+
+        if (!$this->cooldowns->exists($weeklyCooldownKey)) {
+            $this->cooldowns->set($weeklyCooldownKey, 0);
+            $this->cooldowns->save();
+        }
+
+        if (!$this->cooldowns->exists($monthlyCooldownKey)) {
+            $this->cooldowns->set($monthlyCooldownKey, 0);
+            $this->cooldowns->save();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# DailyRewardsPE
+DailyRewards plugin is designed for Pocketmine-MP.
+With this plugin players could now do one of the following commands "/daily", "/weekly" and "/monthly"to get 1 randomly selected items from the rewards list. The loot are divided by types such as daily will only retrieve the items from the daily items list ect. Take a look at "rewards.yml". Custom Enchantments are supported(PiggyCE) as well as my BankNotes plugin. [BANKNOTESPLUS-CLICK ME](https://poggit.pmmp.io/ci/Terpz710/BankNotesPlus/~)
+
+# Todo
+
+- [x] Add messages.yml to edit the messages
+- [x] Banknotes support(Will only supprt my BankNotes. [BANKNOTESPLUS-CLICK ME](https://poggit.pmmp.io/ci/Terpz710/BankNotesPlus/~)
+- [x] Add weekly rewards.
+- [x] Add monthy rewards.
+- [x] Add different loot tiers for daily, weekly and monthly.
+- [ ] Add UI support.
