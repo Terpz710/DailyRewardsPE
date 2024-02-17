415b525c973e2aed65234753137190563a7b7296
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-02-17 12:12:10 -0800

    minor fixes

--- src/Terpz710/DailyRewardsPE/Commands/DailyCommand.php
+++ src/Terpz710/DailyRewardsPE/Commands/DailyCommand.php
@@ -73,6 +73,11 @@ class DailyCommand extends Command implements PluginOwned {
     }
 
     private function giveReward(Player $player, array $rewardData) {
+        if ($this->bankNotesPlus === null || !($this->bankNotesPlus instanceof BankNotesPlus)) {
+            $player->sendMessage("BankNotesPlus plugin is not installed, Skipping bank note reward!");
+            return;
+        }
+
         if (isset($rewardData['is_bank_note']) && $rewardData['is_bank_note']) {
             $amount = $rewardData['amount'];
             $this->bankNotesPlus->convertToBankNote($player, $amount);
