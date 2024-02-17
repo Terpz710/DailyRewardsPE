f6d72ce5298f9b621da24fb0b8c3a227374af653
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-02-16 21:45:03 -0800

    fixes

--- src/Terpz710/DailyRewardsPE/Commands/WeeklyCommand.php
+++ src/Terpz710/DailyRewardsPE/Commands/WeeklyCommand.php
@@ -24,10 +24,10 @@ class WeeklyCommand extends Command implements PluginOwned {
     private $cooldowns;
     private $messages;
 
-    public function __construct(Main $plugin, BankNotesPlus $bankNotesPlus) {
+    public function __construct(Main $plugin) {
         parent::__construct("weekly", "Claim your weekly items");
         $this->plugin = $plugin;
-        $this->bankNotesPlus = $bankNotesPlus;
+        $this->bankNotesPlus = $this->plugin->getServer()->getPluginManager()->getPlugin("BankNotesPlus");
         $this->cooldowns = $plugin->getCooldowns();
         $this->messages = $plugin->getMessagesConfig();
         $this->setPermission("dailyrewardsplus.command.weekly");
